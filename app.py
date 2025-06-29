8047b0fa85e45880d2224bbd60eb17f66db36ee8
Islam Magdy <46000430+IslamMagdy1137@users.noreply.github.com>
2025-06-29 23:23:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer,KNNImputer
+from datasist.structdata import detect_outliers
+from sklearn.model_selection import train_test_split
+import category_encoders as ce
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PowerTransformer
 import joblib
 import streamlit as st
 
